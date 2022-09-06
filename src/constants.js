f378579c0fd0eb8e3dbbaa617a30c9cbda9b9f32
Mateusz Lubiński <mateuszlubinski@hotmail.com>
2022-09-06 22:33:47 +0200

    create constants.js, add languages and audio codecs

---
+++ src/constants.js
@@ -0,0 +1,119 @@
+const languages = [{
+    id: "ar-eg", name: "Arabic (Egypt)", voices: [{name: "Oda", gender: "female", default: true},]
+}, {
+    id: "ar-sa", name: "Arabic (Saudi Arabia)", voices: [{name: "Salim", gender: "male", default: true},]
+}, {id: "bg-bg", name: "Bulgarian", voices: [{name: "Dimo", gender: "male", default: true},]}, {
+    id: "ca-es", name: "Catalan", voices: [{name: "Rut", gender: "female", default: true},]
+}, {
+    id: "zh-cn", name: "Chinese (China)", voices: [{name: "Luli", gender: "female", default: true}, {
+        name: "Shu", gender: "female", default: false
+    }, {name: "Chow", gender: "female", default: false}, {name: "Wang", gender: "male", default: false},]
+}, {
+    id: "zh-hk", name: "Chinese (Hong Kong)", voices: [{name: "Jia", gender: "female", default: true}, {
+        name: "Xia", gender: "female", default: false
+    }, {name: "Chen", gender: "male", default: false},]
+}, {
+    id: "zh-tw", name: "Chinese (Taiwan)", voices: [{name: "Akemi", gender: "female", default: true}, {
+        name: "Lin", gender: "female", default: false
+    }, {name: "Lee", gender: "male", default: false},]
+}, {id: "hr-hr", name: "Croatian", voices: [{name: "Nikola", gender: "male", default: true},]}, {
+    id: "cs-cz", name: "Czech", voices: [{name: "Josef", gender: "male", default: true},]
+}, {id: "da-dk", name: "Danish", voices: [{name: "Freja", gender: "female", default: true},]}, {
+    id: "nl-be", name: "Dutch (Belgium)", voices: [{name: "Daan", gender: "male", default: true},]
+}, {
+    id: "nl-nl",
+    name: "Dutch (Netherlands)",
+    voices: [{name: "Lotte", gender: "female", default: true}, {name: "Bram", gender: "male", default: false},]
+}, {
+    id: "en-au", name: "English (Australia)", voices: [{name: "Zoe", gender: "female", default: true}, {
+        name: "Isla", gender: "female", default: false
+    }, {name: "Evie", gender: "female", default: false}, {name: "Jack", gender: "male", default: false},]
+}, {
+    id: "en-ca", name: "English (Canada)", voices: [{name: "Rose", gender: "female", default: true}, {
+        name: "Clara", gender: "female", default: false
+    }, {name: "Emma", gender: "female", default: false}, {name: "Mason", gender: "male", default: false},]
+}, {
+    id: "en-gb", name: "English (Great Britain)", voices: [{name: "Alice", gender: "female", default: true}, {
+        name: "Nancy", gender: "female", default: false
+    }, {name: "Lily", gender: "female", default: false}, {name: "Harry", gender: "male", default: false},]
+}, {
+    id: "en-in", name: "English (India)", voices: [{name: "Eka", gender: "female", default: true}, {
+        name: "Jai", gender: "female", default: false
+    }, {name: "Ajit", gender: "male", default: false},]
+}, {id: "en-ie", name: "English (Ireland)", voices: [{name: "Oran", gender: "male", default: true},]}, {
+    id: "en-us", name: "English (United States)", voices: [{name: "Linda", gender: "female", default: true}, {
+        name: "Amy", gender: "female", default: false
+    }, {name: "Mary", gender: "female", default: false}, {name: "John", gender: "male", default: false}, {
+        name: "Mike", gender: "male", default: false
+    },]
+}, {id: "fi-fi", name: "Finnish", voices: [{name: "Aada", gender: "female", default: true},]}, {
+    id: "fr-ca", name: "French (Canada)", voices: [{name: "Emile", gender: "female", default: true}, {
+        name: "Olivia", gender: "female", default: false
+    }, {name: "Logan", gender: "female", default: false}, {name: "Felix", gender: "male", default: false},]
+}, {
+    id: "fr-fr", name: "French (France)", voices: [{name: "Bette", gender: "female", default: true}, {
+        name: "Iva", gender: "female", default: false
+    }, {name: "Zola", gender: "female", default: false}, {name: "Axel", gender: "male", default: false},]
+}, {id: "fr-ch", name: "French (Switzerland)", voices: [{name: "Theo", gender: "male", default: true},]}, {
+    id: "de-at", name: "German (Austria)", voices: [{name: "Lukas", gender: "male", default: true},]
+}, {
+    id: "de-de", name: "German (Germany)", voices: [{name: "Hanna", gender: "female", default: true}, {
+        name: "Lina", gender: "female", default: false
+    }, {name: "Jonas", gender: "male", default: false},]
+}, {id: "de-ch", name: "German (Switzerland)", voices: [{name: "Tim", gender: "male", default: true},]}, {
+    id: "el-gr", name: "Greek", voices: [{name: "Neo", gender: "male", default: true},]
+}, {id: "he-il", name: "Hebrew", voices: [{name: "Rami", gender: "male", default: true},]}, {
+    id: "hi-in",
+    name: "Hindi",
+    voices: [{name: "Puja", gender: "female", default: true}, {name: "Kabir", gender: "male", default: false},]
+}, {id: "hu-hu", name: "Hungarian", voices: [{name: "Mate", gender: "male", default: true},]}, {
+    id: "id-id", name: "Indonesian", voices: [{name: "Intan", gender: "male", default: true},]
+}, {
+    id: "it-it", name: "Italian", voices: [{name: "Bria", gender: "female", default: true}, {
+        name: "Mia", gender: "female", default: false
+    }, {name: "Pietro", gender: "male", default: false},]
+}, {
+    id: "ja-jp", name: "Japanese", voices: [{name: "Hina", gender: "female", default: true}, {
+        name: "Airi", gender: "female", default: false
+    }, {name: "Fumi", gender: "female", default: false}, {name: "Akira", gender: "male", default: false},]
+}, {id: "ko-kr", name: "Korean", voices: [{name: "Nari", gender: "female", default: true},]}, {
+    id: "ms-my", name: "Malay", voices: [{name: "Aqil", gender: "male", default: true},]
+}, {
+    id: "nb-no",
+    name: "Norwegian",
+    voices: [{name: "Marte", gender: "female", default: true}, {name: "Erik", gender: "male", default: false},]
+}, {
+    id: "pl-pl",
+    name: "Polish",
+    voices: [{name: "Julia", gender: "female", default: true}, {name: "Jan", gender: "male", default: false},]
+}, {
+    id: "pt-br", name: "Portuguese (Brazil)", voices: [{name: "Marcia", gender: "female", default: true}, {
+        name: "Ligia", gender: "female", default: false
+    }, {name: "Yara", gender: "female", default: false}, {name: "Dinis", gender: "male", default: false},]
+}, {
+    id: "pt-pt", name: "Portuguese (Portugal)", voices: [{name: "Leonor", gender: "female", default: true},]
+}, {id: "ro-ro", name: "Romanian", voices: [{name: "Doru", gender: "male", default: true},]}, {
+    id: "ru-ru", name: "Russian", voices: [{name: "Olga", gender: "female", default: true}, {
+        name: "Marina", gender: "female", default: false
+    }, {name: "Peter", gender: "male", default: false},]
+}, {id: "sk-sk", name: "Slovak", voices: [{name: "Beda", gender: "male", default: true},]}, {
+    id: "sl-si", name: "Slovenian", voices: [{name: "Vid", gender: "male", default: true},]
+}, {
+    id: "es-mx", name: "Spanish (Mexico)", voices: [{name: "Juana", gender: "female", default: true}, {
+        name: "Silvia", gender: "female", default: false
+    }, {name: "Teresa", gender: "female", default: false}, {name: "Jose", gender: "male", default: false},]
+}, {
+    id: "es-es", name: "Spanish (Spain)", voices: [{name: "Camila", gender: "female", default: true}, {
+        name: "Sofia", gender: "female", default: false
+    }, {name: "Luna", gender: "female", default: false}, {name: "Diego", gender: "male", default: false},]
+}, {
+    id: "sv-se",
+    name: "Swedish",
+    voices: [{name: "Molly", gender: "female", default: true}, {name: "Hugo", gender: "male", default: false},]
+}, {id: "ta-in", name: "Tamil", voices: [{name: "Sai", gender: "male", default: true},]}, {
+    id: "th-th", name: "Thai", voices: [{name: "Ukrit", gender: "male", default: true},]
+}, {id: "tr-tr", name: "Turkish", voices: [{name: "Omer", gender: "male", default: true},]}, {
+    id: "vi-vn", name: "Vietnamese", voices: [{name: "Chi", gender: "male", default: true},]
+}];
+
+const audioCodecs = [{id: "MP3"}, {id: "WAV"}, {id: "AAC"}, {id: "OGG"}, {id: "CAF"}];
